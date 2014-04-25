73e47d11bc0f1d1ed29c90076239b2fc3cbaba5f
Julio Acuña <urkonn@gmail.com>
2014-04-24 19:48:48 -0500

    Error tooltips

--- js/valida.js
+++ js/valida.js
@@ -1,9 +1,10 @@
-$( document ).ready(function() {
-    $("#signup").validate({
+// Basado en https://gist.github.com/johnnyreilly/5867188#file-demotooltip-html
+$(document).ready(function(){
+    $("signup").validate({
         rules: {
             name: {
                 required: true,
-                rangelength: [2, 50]
+                rangelength: [3, 50]
             },
             email: {
                 required: true,
@@ -21,7 +22,7 @@ $( document ).ready(function() {
         messages: {
             name: {
                 required: "Por favor escribe tu nombre",
-                rangelength: "Debe tener entre 2 y 50 caracteres"
+                rangelength: "Debe tener entre 3 y 50 caracteres"
             },
             email: {
                 required: "Necesitamos tu email para contactarte"
@@ -30,6 +31,31 @@ $( document ).ready(function() {
                 required: "Por favor escribe el nombre de tu empresa",
                 rangelength: "Debe tener entre 2 y 150 caracteres"
             },
+        },
+    
+        showErrors: function(errorMap, errorList) {
+
+            // Clean up any tooltips for valid elements
+            $.each(this.validElements(), function (index, element) {
+            var $element = $(element);
+                $element.data("title", "") // Clear the title - there is no error associated anymore
+                .removeClass("error")
+                .tooltip("destroy");
+            });
+     
+            // Create new tooltips for invalid elements
+            $.each(errorList, function (index, error) {
+            var $element = $(error.element);
+     
+            $element.tooltip("destroy") // Destroy any pre-existing tooltip so we can repopulate with new tooltip content
+                .data("title", error.message)
+                .addClass("error")
+                .tooltip(); // Create a new tooltip based on the error messsage we just set in the title
+            });
+        },
+ 
+        submitHandler: function(form) {
+            form.submit();
         }
     });
 });
